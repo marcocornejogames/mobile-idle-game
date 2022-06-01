dc61e340312988afb3c61601999bf6457666af20
Marco <manto.cornejo@gmail.com>
2022-06-01 18:28:10 -0400

    Camera Restrained to 2D Background

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -7,6 +7,7 @@ public class CameraMovement : MonoBehaviour
 {
     [Header("Component References")]
     [SerializeField] private Camera _mainCamera;
+    [SerializeField] private SpriteRenderer _backgroundRenderer;
 
     [Header("Customization")]
     [SerializeField] private float _minZoom = 3f;
@@ -20,14 +21,32 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private bool _touchTwoIsPressed;
     [SerializeField] private float _currentPinchDistance;
     
+    //Dragging and zooming logic variables
     private Vector3 dragOrigin;
     private bool isDragging = false;
     private float startPinchDistance;
     private bool isZooming = false;
 
+    //Background 
+    private float _backgroundMinX;
+    private float _backgroundMaxX;
+    private float _backgroundMinY;
+    private float _backgroundMaxY;
+
+
     private void Awake()
     {
+        //Find components
         _mainCamera = GetComponent<Camera>();
+
+
+        //Assign background variables
+        _backgroundMinX = _backgroundRenderer.transform.position.x - _backgroundRenderer.bounds.size.y / 2;
+        _backgroundMinY = _backgroundRenderer.transform.position.y - _backgroundRenderer.bounds.size.y / 2;
+        _backgroundMaxX = _backgroundRenderer.transform.position.x + _backgroundRenderer.bounds.size.x / 2;
+        _backgroundMaxY = _backgroundRenderer.transform.position.y + _backgroundRenderer.bounds.size.y / 2;
+
+
     }
     private void Update()
     {
@@ -47,7 +66,7 @@ public class CameraMovement : MonoBehaviour
         if(isDragging && !_touchTwoIsPressed)
         {
             Vector3 difference = dragOrigin - _mainCamera.ScreenToWorldPoint(_touchOnePosition);
-            _mainCamera.transform.position += difference;
+            _mainCamera.transform.position = ClampCamera(_mainCamera.transform.position + difference);
         }
 
         if (!_touchOneIsPressed || _touchTwoIsPressed) isDragging = false;
@@ -74,8 +93,26 @@ public class CameraMovement : MonoBehaviour
     private void ApplyZoom(float increment)
     {
         _mainCamera.orthographicSize = Mathf.Clamp(_mainCamera.orthographicSize - increment, _minZoom, _maxZoom);
+        _mainCamera.transform.position = ClampCamera(_mainCamera.transform.position);
     }
 
+    private Vector3 ClampCamera(Vector3 targetPosition)
+    {
+        float camHeight = _mainCamera.orthographicSize;
+        float camWidth = _mainCamera.orthographicSize * _mainCamera.aspect;
+
+        float minX = _backgroundMinX + camWidth;
+        float maxX = _backgroundMaxX - camWidth;
+
+        float minY = _backgroundMinY + camHeight;
+        float maxY = _backgroundMaxY - camHeight;
+
+        float newX = Mathf.Clamp(targetPosition.x, minX, maxX);
+        float newY = Mathf.Clamp(targetPosition.y, minY, maxY);
+
+        return new Vector3(newX, newY, targetPosition.z);
+
+    }
     //EVENT LISTENERS _________________________________________________
     public void UpdateTouchOnePosition(Vector2 position)
     {
